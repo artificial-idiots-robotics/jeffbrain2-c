e9f51749f274a7621a53c9429390b3dba93ba42f
Aiden <aiden.pachel@isd31.net>
2025-10-22 11:34:32 -0500

    I done fixed the image renderer

--- src/interface.cpp
+++ src/interface.cpp
@@ -8,8 +8,10 @@ lv_obj_t * temp_label_arm = NULL;
 lv_obj_t * temp_label_clw = NULL;
 
 lv_obj_t * auton_status_label = NULL;
+lv_obj_t * toggle_display_image = NULL;
 
 int selected_auton = 0;
+
 
 static void auton_btn_click_action(lv_event_t * e) {
     lv_obj_t * btn = (lv_obj_t *)lv_event_get_target(e);
@@ -109,13 +111,13 @@ void create_auton_tab(lv_obj_t * parent_tab) {
     lv_label_set_text(none_label, "None");
 
     lv_obj_t * skills_btn = lv_btn_create(parent_tab);
-    lv_obj_set_pos(skills_btn, 10, 50);
+    lv_obj_set_pos(skills_btn, 230, 50);
     lv_obj_set_size(skills_btn, 100, 40);
     lv_obj_set_user_data(skills_btn, (void*)5);
     lv_obj_add_event_cb(skills_btn, auton_btn_click_action, LV_EVENT_CLICKED, NULL);
 
     lv_obj_t * skills_label = lv_label_create(skills_btn);
-    lv_label_set_text(skills_label, "RED Left");
+    lv_label_set_text(skills_label, "Skills");
 
     lv_obj_t * red_left_btn = lv_btn_create(parent_tab);
     lv_obj_set_pos(red_left_btn, 10, 110);
@@ -136,7 +138,7 @@ void create_auton_tab(lv_obj_t * parent_tab) {
     lv_label_set_text(red_right_label, "RED Right");
 
     lv_obj_t * blu_left_btn = lv_btn_create(parent_tab);
-    lv_obj_set_pos(blu_left_btn, 10, 210);
+    lv_obj_set_pos(blu_left_btn, 10, 170);
     lv_obj_set_size(blu_left_btn, 100, 40);
     lv_obj_set_user_data(blu_left_btn, (void*)3);
     lv_obj_add_event_cb(blu_left_btn, auton_btn_click_action, LV_EVENT_CLICKED, NULL);
@@ -145,7 +147,7 @@ void create_auton_tab(lv_obj_t * parent_tab) {
     lv_label_set_text(blu_left_label, "BLU Left");
 
     lv_obj_t * blu_right_btn = lv_btn_create(parent_tab);
-    lv_obj_set_pos(blu_right_btn, 230, 210);
+    lv_obj_set_pos(blu_right_btn, 230, 170);
     lv_obj_set_size(blu_right_btn, 100, 40);
     lv_obj_set_user_data(blu_right_btn, (void*)4);
     lv_obj_add_event_cb(blu_right_btn, auton_btn_click_action, LV_EVENT_CLICKED, NULL);
@@ -158,27 +160,42 @@ static void image_button_action(lv_event_t * e) {
     lv_obj_t * btn = (lv_obj_t *)lv_event_get_target(e); 
     
     if (lv_event_get_code(e) == LV_EVENT_CLICKED) { 
-        lv_obj_set_style_bg_color(btn, LV_COLOR_MAKE(255, 255, 0), LV_PART_MAIN | LV_STATE_PRESSED);
+        
+        if (lv_obj_has_flag(toggle_display_image, LV_OBJ_FLAG_HIDDEN)) {
+            lv_obj_clear_flag(toggle_display_image, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_set_style_bg_color(btn, LV_COLOR_MAKE(0, 200, 0), LV_PART_MAIN | LV_STATE_DEFAULT);
+        } else {
+            lv_obj_add_flag(toggle_display_image, LV_OBJ_FLAG_HIDDEN);
+            lv_obj_set_style_bg_color(btn, LV_COLOR_MAKE(255, 0, 0), LV_PART_MAIN | LV_STATE_DEFAULT);
+        }
     }
 }
 
 void create_image_tab(lv_obj_t * parent_tab) {
     lv_obj_t * image_btn = lv_btn_create(parent_tab);
     lv_obj_set_pos(image_btn, 10, 10);
-    lv_obj_set_size(image_btn, 100, 100);
+    lv_obj_set_size(image_btn, 100, 40);
     lv_obj_add_event_cb(image_btn, image_button_action, LV_EVENT_CLICKED, NULL);
 
-    lv_obj_set_style_bg_color(image_btn, LV_COLOR_MAKE(0, 0, 255), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_t * image_label = lv_label_create(image_btn);
+    lv_label_set_text(image_label, "Minnow");
+
+    lv_obj_set_style_bg_color(image_btn, LV_COLOR_MAKE(255, 0, 0), LV_PART_MAIN | LV_STATE_DEFAULT);
 
     extern const lv_img_dsc_t creature; 
-    lv_obj_t * image_child = lv_img_create(image_btn);
 
-    lv_img_set_src(image_child, &creature); 
-    lv_obj_set_align(image_child, LV_ALIGN_CENTER);
+    toggle_display_image = lv_img_create(parent_tab);
+    lv_img_set_src(toggle_display_image, &creature);
+    lv_obj_move_foreground(toggle_display_image);
+
+    lv_obj_align(toggle_display_image, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_add_flag(toggle_display_image, LV_OBJ_FLAG_HIDDEN);
 }
 
 void initialize_interface() {
     main_tabview = lv_tabview_create(lv_scr_act());
+    lv_obj_t * tab_bar = lv_tabview_get_tab_bar(main_tabview);
+    lv_obj_set_height(tab_bar, 30);
     
     lv_obj_t * temp_tab = lv_tabview_add_tab(main_tabview, "Monitor");
     create_temp_tab(temp_tab);
@@ -186,5 +203,8 @@ void initialize_interface() {
     lv_obj_t * auton_tab = lv_tabview_add_tab(main_tabview, "Auton");
     create_auton_tab(auton_tab);
     
+    lv_obj_t * image_tab = lv_tabview_add_tab(main_tabview, "Images");
+    create_image_tab(image_tab);
+
     lv_tabview_set_act(main_tabview, 1, LV_ANIM_OFF); 
 }
