6399dbc53eb7faaf76d4b16b8fea5d33f683e33c
Aiden <aiden.pachel@isd31.net>
2025-10-28 08:12:18 -0500

    added more drive functions, reversed motors

--- src/globals.cpp
+++ src/globals.cpp
@@ -3,7 +3,7 @@
 pros::Motor drivebase_left(1);
 pros::Motor drivebase_right(2);
 pros::Motor arm_motor(4);
-pros::Motor claw_motor(5);
+pros::Motor claw_motor(6);
 
 pros::Controller master_controller(pros::E_CONTROLLER_MASTER);
 pros::Controller partner_controller(pros::E_CONTROLLER_PARTNER);
--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ void initialize() {
     arm_motor.set_gearing(pros::v5::MotorGears::red);
     claw_motor.set_gearing(pros::v5::MotorGears::green);
 
-    drivebase_left.set_reversed(false);
-    drivebase_right.set_reversed(false);
+    drivebase_left.set_reversed(true);
+    drivebase_right.set_reversed(true);
     arm_motor.set_reversed(false);
     claw_motor.set_reversed(false);
 
@@ -39,13 +39,29 @@ void opcontrol() {
             int turn = master_controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
             drivebase_left.move(((turn * 0.6) + dir) * 0.8);
             drivebase_right.move(((turn * 0.6) - dir) * 0.8);
-            pros::delay(5);
         } else if (control_mode == 1) {
             int leftdrive = master_controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
             int rightdrive = master_controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
             drivebase_left.move(leftdrive);
             drivebase_right.move(-rightdrive);
-            pros::delay(5);
         }
+
+        if (master_controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+            claw_motor.move(127);
+        } else if (master_controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            claw_motor.move(-127);
+        } else {
+            claw_motor.brake();
+        }
+
+        if (master_controller.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
+            arm_motor.move(127);
+        } else if (master_controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+            arm_motor.move(-127);
+        } else {
+            arm_motor.brake();
+        }
+
+        pros::delay(5);
 	}
 }
